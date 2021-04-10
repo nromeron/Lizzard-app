8fd239990d5a0aba11c97d9d6f4eba9f7261220e
Nicolas <niklas799@hotmail.com>
2021-04-10 17:53:41 -0500

    first commit

---
+++ src/main/java/com/unal/lizzard/model/Role.java
@@ -0,0 +1,35 @@
+package com.unal.lizzard.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Role(String name) {
+        super();
+        this.name = name;
+    }
+}
